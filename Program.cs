66f90e75f8979c30b76b95568c107e077268b2a6
EtinRuyi <omoruyi.idowu@yahoo.com>
2023-09-09 10:39:59 +0100

    GPA Calculator

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week0Task_GPACalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new PrintTables();
+            obj.PrintMyTable();
+
+
+            Console.ReadLine();
+        }
+    }
+}
